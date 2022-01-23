73e104287a554fa0ffcde958a067f0cf6b2b1e9d
tpolinas <56580349+tpolinas@users.noreply.github.com>
2022-01-23 20:16:20 +0300

    some new fixes + like animation

--- VK/View/Reusable Cells/Collection Cell/ProfileCollectionCell.swift
+++ VK/View/Reusable Cells/Collection Cell/ProfileCollectionCell.swift
@@ -13,15 +13,32 @@ class ProfileCollectionCell: UICollectionViewCell {
     @IBOutlet var age: UILabel!
     @IBOutlet var likeButton: UIButton!
     
+    var isLiked: Bool = false
+    
     @IBAction func like(_ sender: Any) {
-        if likeButton.titleLabel?.text == "0" {
-            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-            likeButton.setTitle("1", for: .normal)
-        } else {
-            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
-            likeButton.setTitle("0", for: .normal)
-        }
+        
+        animate()
     }
+    
+    private func animate() {
+        UIButton.animate(withDuration: 0.1, animations: {
+            switch self.isLiked {
+            case false:
+                self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                self.likeButton.setTitle("1", for: .normal)
+                self.isLiked = true
+            default:
+                self.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                self.likeButton.setTitle("0", for: .normal)
+                self.isLiked = false
+            }
+          self.transform = self.transform.scaledBy(x: 0.8, y: 0.8)
+        }, completion: { _ in
+            UIButton.animate(withDuration: 0.1, animations: {
+            self.transform = CGAffineTransform.identity
+          })
+        })
+      }
     
     func configure(
         name: String,
